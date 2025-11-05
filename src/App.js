0c9cfca72d54b54d69b1fa18e45b876439bfbf1a
Juan Puente <juanp220077@gmail.com>
2025-11-05 03:46:17 -0500

    Conexion final exitosa al Backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+//import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Importa tus componentes/páginas
 import Home from './pages/Home'; // Si existe
